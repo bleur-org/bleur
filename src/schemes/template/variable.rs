6d3a8a36e9c3aa88eb30679fcc2d93fc1042e854
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-01-09 12:24:25 +0500

    chore: variable execution

--- src/schemes/template/variable.rs
+++ src/schemes/template/variable.rs
@@ -2,17 +2,17 @@ use crate::{
     execute::task::{Task, ToTask},
     Error, Result,
 };
-use log::debug;
 use serde::{Deserialize, Serialize};
 use std::{
-    fs,
+    fs::{self, OpenOptions},
+    io::Write,
     path::{Path, PathBuf},
 };
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, PartialOrd, Eq, Ord)]
 pub struct Variable {
     placeholder: String,
-    // message: String,
+    message: String,
     types: String,
     default: String,
     source: PathBuf,
@@ -20,15 +20,20 @@ pub struct Variable {
 
 impl Variable {
     pub fn execute(&self) -> Result<()> {
-        // let prompt = inquire::Text::new("Choose the template you would like to bootstrap:")
-        //     .with_default(&self.default)
-        //     .with_initial_value(&self.default)
-        //     .prompt()
-        //     .map_err(Error::CantParseUserPrompt)?;
+        let prompt = inquire::Text::new(&self.message)
+            .with_default(&self.default)
+            .with_placeholder(&self.default)
+            .prompt()
+            .map_err(Error::CantParseUserPrompt)?;
 
-        // let contents = fs::read_to_string(self.source);
+        let contents = fs::read_to_string(self.source.clone())?.replace(&self.placeholder, &prompt);
 
-        debug!("source is: {:?}", self.source);
+        let mut file = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(&self.source)?;
+
+        file.write_all(contents.as_bytes())?;
 
         Ok(())
     }
@@ -38,6 +43,7 @@ impl ToTask for Variable {
     fn to_task(self, path: &Path) -> Task {
         Task::Rename(Variable {
             placeholder: self.placeholder,
+            message: self.message,
             types: self.types,
             default: self.default,
             source: path.join(self.source),
